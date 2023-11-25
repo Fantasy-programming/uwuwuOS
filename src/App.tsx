2e89d276fe0f3f0a8da09bdb5e46341eb4361554
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2023-11-25 14:09:17 +0000

    test123

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { Rnd } from "react-rnd";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Rnd
+        default={{
+          x: 0,
+          y: 0,
+          width: 320,
+          height: 200,
+        }}
+      >
+        Rnd
+      </Rnd>
     </>
   );
 }
