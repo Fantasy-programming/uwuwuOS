480de129733c2ec871b78491a2905c3bcd0a7906
Fantasy <37631123+Fantasy-programming@users.noreply.github.com>
2023-12-13 21:37:26 +0000

    Chore: create store

---
+++ src/stores/windowStore.ts
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+
+export interface WindowState {
+  id: string;
+  name: string;
+  appName: string;
+  pos_x?: number;
+  pos_y?: number;
+  width?: string;
+  height?: string;
+}
+
+interface WindowsState {
+  processes: number;
+  windows: WindowState[];
+}
+
+interface WindowsActions {
+  spawnProcess: (info: WindowState) => void;
+  killProcess: (id: string) => void;
+}
+
+export const usewindowStore = create<WindowsState & WindowsActions>()(
+  immer((set) => ({
+    processes: 0,
+    windows: [],
+    spawnProcess: (info: WindowState) =>
+      set((state) => {
+        state.processes + 1, state.windows.push(info);
+      }),
+    killProcess: (id: string) =>
+      set((state) => {
+        state.processes - 1,
+          (state.windows = state.windows.filter((window) => window.id !== id));
+      }),
+  })),
+);
